701842fa00ce6f9d6f1503824c9c367f97897417
mamun2519 <juborajislam46@gmail.com>
2025-12-13 22:27:28 +0600

    feat: join config file from server

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import type { Application, Request, Response } from "express";
 import express from "express";
 const app: Application = express();
 
+// Health check endpoint
 app.get("/health", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
