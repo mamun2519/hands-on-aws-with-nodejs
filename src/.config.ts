25f2cc10d78973cd95f78190776be714861127b1
mamun2519 <juborajislam46@gmail.com>
2025-12-13 22:24:19 +0600

    feat: added to config file

--- src/.config.ts
+++ src/.config.ts
@@ -6,12 +6,4 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url: process.env.DB_URL,
-  bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-  jwt: {
-    secret_token: process.env.JWT_SECRET,
-    expire_in: process.env.JWT_EXPIRES_IN,
-    refresh_token: process.env.REFRESH_SECRET_TOKEN,
-    refresh_expire_in: process.env.REFRESH_EXPIRE_IN,
-  },
 };
