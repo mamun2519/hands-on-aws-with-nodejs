b1dac16e8039382e02776d757064ac46f5647cd9
mamun2519 <juborajislam46@gmail.com>
2025-12-13 22:27:42 +0600

    faet: added to middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import type { Application, Request, Response } from "express";
 import express from "express";
 const app: Application = express();
 
+// middleware--------
+app.use([express.json(), express.urlencoded({ extended: true })]);
 // Health check endpoint
 app.get("/health", (req: Request, res: Response) => {
   res.send("Hello, World!");
