62dfd89305e8d49bfdc71fc34feaa3cc93a65eaf
mamun2519 <juborajislam46@gmail.com>
2025-12-13 22:20:57 +0600

    added to health api

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import type { Application, Request, Response } from "express";
 import express from "express";
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/health", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
