24e3f33b04e40dcbb3272be70753cc2d8381fe3c
mamun2519 <juborajislam46@gmail.com>
2025-12-13 22:28:46 +0600

    feat: config to cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import type { Application, Request, Response } from "express";
 import express from "express";
+import cors from "cors";
 const app: Application = express();
 
 // middleware--------
 app.use([express.json(), express.urlencoded({ extended: true })]);
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // Health check endpoint
 app.get("/health", (req: Request, res: Response) => {
   res.send("Hello, World!");
