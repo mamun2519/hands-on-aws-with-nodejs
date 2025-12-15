098f37d4c89df37055c22fbaff160cb7a5bba0a6
mamun2519 <juborajislam46@gmail.com>
2025-12-15 21:58:59 +0600

    feat: added to s3 config file

--- src/aws-services/S3/config.ts
+++ src/aws-services/S3/config.ts
@@ -2,4 +2,8 @@ import AWS from "aws-sdk";
 import config from "../../config";
 
 // AWS S3 Configuration
-const s3 = new AWS.S3({});
+const s3 = new AWS.S3({
+  accessKeyId: config.aws.s3.accessKeyId,
+  secretAccessKey: config.aws.s3.secretAccessKey,
+  region: config.aws.s3.region,
+});
