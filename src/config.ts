3c175ed8656ac2d5425f1d626cc3aa7b6acb9bbe
mamun2519 <juborajislam46@gmail.com>
2025-12-15 21:57:05 +0600

    feat: added to s3 config in the config file

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,12 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
+  aws: {
+    s3: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+      s3BucketName: process.env.S3_BUCKET_NAME,
+    },
+  },
 };
