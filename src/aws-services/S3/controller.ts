dfebd3e2d1f3275bc60ea52aca43c1d46f9fabca
mamun2519 <juborajislam46@gmail.com>
2025-12-15 22:06:20 +0600

    feat: added to get all s3 file services

--- src/aws-services/S3/controller.ts
+++ src/aws-services/S3/controller.ts
@@ -4,5 +4,8 @@ import { S3Service } from "./services";
 const getAllS3Files = async (req: Request, res: Response) => {
   const result = await S3Service.getAllS3Files();
 
-  res;
+  res.status(200).json({
+    message: "S3 Files fetched successfully",
+    data: result,
+  });
 };
