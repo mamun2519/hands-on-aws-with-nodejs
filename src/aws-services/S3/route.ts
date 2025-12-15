94ddd579cd6cb7cd6d3a13ba12b43bc02b9071c7
mamun2519 <juborajislam46@gmail.com>
2025-12-15 22:06:49 +0600

    feat: added to get all s3 file services

--- src/aws-services/S3/route.ts
+++ src/aws-services/S3/route.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { S3Controller } from "./controller";
 const router = express.Router();
 
 router.get("/s3-health", (req, res) => {
   res.send("S3 Service is healthy");
 });
+router.get("/s3-files", S3Controller.getAllS3Files);
 
 export const S3Route = router;
