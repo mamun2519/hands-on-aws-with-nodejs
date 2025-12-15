13476276cd79a277a03a97a3dedf25028527c13d
mamun2519 <juborajislam46@gmail.com>
2025-12-15 22:50:25 +0600

    feat: change to s3 promise

--- src/aws-services/S3/services.ts
+++ src/aws-services/S3/services.ts
@@ -5,7 +5,7 @@ const getAllS3Files = async () => {
   const params = {
     Bucket: config.aws.s3.s3BucketName ?? "",
   };
-  const result = s3.listObjectsV2(params);
+  const result = s3.listObjectsV2(params).promise();
   console.log("S3 Files:", result);
   return result;
 };
