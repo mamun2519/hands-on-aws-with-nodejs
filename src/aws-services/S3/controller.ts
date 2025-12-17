7d2a79c40b1eb97886c2ad5a705bade3c571ff0e
mamun2519 <juborajislam46@gmail.com>
2025-12-17 21:50:44 +0600

    feat: added to s3 operation with nodejs

--- src/aws-services/S3/controller.ts
+++ src/aws-services/S3/controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { S3Service } from "./services";
 
 const getAllS3Files = async (req: Request, res: Response) => {
-  const result = await S3Service.getAllS3Files();
+  const result = await S3Service.getAllFilesFormS3Bucket();
 
   res.status(200).json({
     message: "S3 Files fetched successfully",
