4382968c470c2082f2d98bd8825b2459d0189c65
mamun2519 <juborajislam46@gmail.com>
2025-12-17 22:18:54 +0600

    feat: added to s3 operation with nodejs

--- src/aws-services/S3/services.ts
+++ src/aws-services/S3/services.ts
@@ -20,6 +20,7 @@ const getSingleFileFromS3Bucket = async (key: string) => {
     Bucket: config.aws.s3.s3BucketName ?? "",
     Key: key,
   };
+
   const result = await s3.getObject(params).promise();
   return result;
 };
