55fa54c778da823e6ab2b5b622e856560694ee2f
mamun2519 <juborajislam46@gmail.com>
2025-12-20 21:51:26 +0600

    feat: hands on node s3 operation

--- src/aws-services/S3/services.ts
+++ src/aws-services/S3/services.ts
@@ -25,4 +25,9 @@ const getSingleFileFromS3Bucket = async (key: string) => {
   return result;
 };
 
+const uploadFileToS3Bucket = async (
+  key: string,
+  body: Buffer | Uint8Array | Blob | string
+) => {};
+
 // if need one time image view link generation
