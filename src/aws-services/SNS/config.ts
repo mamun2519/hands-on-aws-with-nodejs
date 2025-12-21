c0621c6598837523d576ed719f1dd34b19d057b4
mamun2519 <juborajislam46@gmail.com>
2025-12-21 19:03:12 +0600

    add to sns config object and manage all operation

--- src/aws-services/SNS/config.ts
+++ src/aws-services/SNS/config.ts
@@ -1,7 +1,22 @@
 import config from "../../config";
+import {
+  SNSClient,
+  CreateTopicCommand,
+  SubscribeCommand,
+  PublishCommand,
+  ListSubscriptionsByTopicCommand,
+  UnsubscribeCommand,
+} from "@aws-sdk/client-sns";
 
 class SNSService {
-      constructor(){
-            this.client = new 
-      }
+  constructor() {
+    this.client = new SNSClient({
+      region: config.aws.sns.region,
+      credentials: {
+        accessKeyId: config.aws.sns.accessKeyId ?? "",
+        secretAccessKey: config.aws.sns.secretAccessKey ?? "",
+      },
+    });
+  }
+    
 }
