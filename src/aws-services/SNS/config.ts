f9b0b5a6e61ffed03fbfa7085b74179f40f2342d
mamun2519 <juborajislam46@gmail.com>
2025-12-21 18:41:39 +0600

    Add sns operation

--- src/aws-services/SNS/config.ts
+++ src/aws-services/SNS/config.ts
@@ -1,7 +1,9 @@
 import config from "../../config";
 import AWS from "aws-sdk";
 const SNS = new AWS.SNS({
-  accessKeyId: config.aws.sns.accessKeyId,
+  accessKeyId: config.aws.s3.accessKeyId,
   secretAccessKey: config.aws.sns.secretAccessKey,
   region: config.aws.sns.region,
 });
+
+export default SNS;
