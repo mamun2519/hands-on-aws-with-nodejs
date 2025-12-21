c2ec9e90054a813ea43b2cfff4e3a44741c08f60
mamun2519 <juborajislam46@gmail.com>
2025-12-21 19:06:19 +0600

    add to sns config object and manage all operation

--- src/aws-services/SNS/config.ts
+++ src/aws-services/SNS/config.ts
@@ -19,10 +19,15 @@ class SNSService {
         secretAccessKey: config.aws.sns.secretAccessKey ?? "",
       },
     });
+    this.createTopic = config.aws.sns.topicArn
+      ? this.createTopic.bind(this)
+      : async () => {
+          throw new Error("SNS Topic ARN is not configured.");
+        };
   }
 
   // ============================================
-  // 1. Create SNS Topic (Production Setup)
+  // 1. Create SNS Topic
   // ============================================
   async createTopic(topicName: string) {
     const command = new CreateTopicCommand({
