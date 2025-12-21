c0cbd8b7e60f7391401cd4d036bbee17f638ba99
mamun2519 <juborajislam46@gmail.com>
2025-12-21 18:45:05 +0600

    Add sns operation

--- src/aws-services/SNS/config.ts
+++ src/aws-services/SNS/config.ts
@@ -2,8 +2,8 @@ import config from "../../config";
 import AWS from "aws-sdk";
 const SNS = new AWS.SNS({
   accessKeyId: config.aws.s3.accessKeyId,
-  secretAccessKey: config.aws.sns.secretAccessKey,
-  region: config.aws.sns.region,
+  secretAccessKey: config.aws.s3.secretAccessKey,
+  region: config.aws.s3.region,
 });
 
 export default SNS;
---
+++ src/aws-services/SNS/sns.services.ts
@@ -0,0 +1,22 @@
+import SNS from "./config";
+
+const sendEmailUsingSNS = async (
+  subject: string,
+  message: string,
+  recipientEmail: string
+) => {
+  const params = {
+    Subject: subject,
+    Message: message,
+    TopicArn: config.aws.sns.topicArn ?? "",
+    MessageAttributes: {
+      "AWS.SNS.SMS.SMSType": {
+        DataType: "String",
+        StringValue: "Promotional",
+      },
+    },
+  };
+
+  const result = await SNS.publish(params).promise();
+  return result;
+};
