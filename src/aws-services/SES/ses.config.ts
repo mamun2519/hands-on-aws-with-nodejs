ff83fb13279e15619f9b0e7c06f2ca3b506dc041
mamun2519 <juborajislam46@gmail.com>
2026-01-01 21:49:42 +0600

    add to Simple Email Service (SES) hands on

--- src/aws-services/SES/ses.config.ts
+++ src/aws-services/SES/ses.config.ts
@@ -4,9 +4,9 @@ class SESConfig {
   constructor() {
     // Configuration settings for AWS SES can be initialized here
     this.region = config.aws.region || "us-east-1";
-    this.accessKeyId = config.aws.accessKeyId;
-    this.secretAccessKey = config.aws.secretAccessKey;
-    this.fromEmail = config.aws.ses.fromEmail;
+    this.accessKeyId = config.aws.accessKeyId ?? "";
+    this.secretAccessKey = config.aws.secretAccessKey ?? "";
+    this.fromEmail = config.aws.ses.fromEmail || "";
     this.forName = config.aws.ses.forName || "YourCompany";
 
     this.validate();
