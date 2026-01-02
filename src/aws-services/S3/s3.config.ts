ffec0c0f2a4948b609dc2e3dff1c0adcecb7761c
mamun2519 <juborajislam46@gmail.com>
2026-01-02 22:18:51 +0600

    modify s3 operation

--- src/aws-services/S3/s3.config.ts
+++ src/aws-services/S3/s3.config.ts
@@ -3,7 +3,7 @@ import config from "../../config";
 
 // AWS S3 Configuration
 const s3 = new AWS.S3({
-  accessKeyId: config.aws.s3.accessKeyId,
+  accessKeyId: config.aws.s3.accessKeyId ?? "",
   secretAccessKey: config.aws.s3.secretAccessKey,
   region: config.aws.s3.region,
 });
